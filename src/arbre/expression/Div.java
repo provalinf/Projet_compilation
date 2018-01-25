2b4df392befe1edd4531b9215b3341aea879fabd
Valentin Bonnal <co.merce@hotmail.fr>
2018-01-25 17:10:13 +0100

    Division

--- src/arbre/expression/Div.java
+++ src/arbre/expression/Div.java
@@ -8,22 +8,30 @@ package arbre.expression;
 
 public class Div extends BinaireArithmetique {
 
-    public Div(Expression gauche, Expression droite) {
-        super(gauche, droite);
-    }
+	public Div(Expression gauche, Expression droite) {
+		super(gauche, droite);
+	}
 
-    @Override
-    public String operateur() {
-        return " / ";
-    }
+	@Override
+	public String operateur() {
+		return " / ";
+	}
 
-    @Override
-    public void verifier() {
+	@Override
+	public void verifier() {
 
-    }
+	}
 
-    @Override
-    public String toMIPS() {
-        return null;
-    }
+	@Override
+	public String toMIPS() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("# Division\n");
+		sb.append(gauche.toMIPS());
+		sb.append("move $v0, $t8\n");
+		sb.append(droite.toMIPS());
+		sb.append("# Divise t8 à v0\n");
+		sb.append("div $v0, $v0, $t8\n");
+		sb.append("move $v0, $t8\n");
+		return sb.toString();
+	}
 }
