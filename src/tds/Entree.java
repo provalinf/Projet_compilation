1a84162fefbad20e28b3d1e969b8cf8253377e36
basile7u <cyril_basile@hotmail.fr>
2018-02-12 11:16:01 +0100

    Classe entree ok

--- src/tds/Entree.java
+++ src/tds/Entree.java
@@ -1,4 +1,14 @@
 package tds;
 
 public class Entree {
+
+    private String id;
+
+    public Entree(String e){
+        id = e;
+    }
+
+    public String getId(){
+        return id;
+    }
 }
--- src/tds/Symbole.java
+++ src/tds/Symbole.java
@@ -1,4 +1,5 @@
 package tds;
 
 public class Symbole {
+
 }
