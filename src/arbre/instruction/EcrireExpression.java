f9d8cf633c8b9d4a52585aa04e545df55a0b70b4
Ska <skalyxe@yahoo.fr>
2018-02-13 08:49:44 +0100

    Correction ecriture

--- src/arbre/instruction/EcrireExpression.java
+++ src/arbre/instruction/EcrireExpression.java
@@ -27,24 +27,16 @@ public class EcrireExpression extends Ecrire{
         sb.append("# Ecrire une expression\n");
         sb.append(exp.toMIPS());
         if(b){
-            sb.append("si" + this.hashCode() + ": beqz $v0, sinon" + this.hashCode() + "\n");
-            sb.append("alors" + this.hashCode() + ": .data\n");
-            sb.append("str1_" + this.hashCode() + ":\t .asciiz \"vrai\"\n");
-            sb.append(".text\n");
+            sb.append("beqz $v0, alors_" + hashCode() + "\n");
+            sb.append("la $a0, 1\n");
+            sb.append("j fin_" + hashCode() + "\n");
+            sb.append("alors_" + hashCode() + " :\n");
+            sb.append("la $a0, 0 \n");
+            sb.append("fin_" + hashCode() + " :\n");
             sb.append("li $v0, 4\n");
-            sb.append("la $a0, str1_" + this.hashCode() + "\n");
-            sb.append("j finsi" + this.hashCode() + "\n");
-            sb.append("sinon" + this.hashCode() + ": .data\n");
-            sb.append("str2_" + this.hashCode() + ":\t .asciiz \"faux\"\n");
-            sb.append(".text\n");
-            sb.append("li $v0, 4\n");
-            sb.append("la $a0, str2_" + this.hashCode() + "\n");
-            sb.append("finsi" + this.hashCode() + ":\n");
-            sb.append("syscall\n");
         }else{
-            sb.append("move $t2, $v0\n");
+            sb.append("move $a0, $v0\n");
             sb.append("li $v0, 1\n");
-            sb.append("move $a0, $t2\n");
         }
         sb.append("syscall\n");
 
