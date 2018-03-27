7badbf49e6dff55bd15f451cda9f2c9fcfff9c9c
Cyril <cyril_basile@hotmail.fr>
2018-03-27 17:33:02 +0200

    FonctionAvecParam cup v1

--- src/arbre/fonctions/FonctionsAvecParam.java
+++ src/arbre/fonctions/FonctionsAvecParam.java
@@ -76,4 +76,69 @@ public class FonctionsAvecParam extends Instruction {
         return sb.toString();
     }
 
+
+    /*public class Fonction extends ArbreAbstrait {
+    private String nom;
+    private BlocDInstructions li;
+    private BlocDInstructions param;
+    private Stack<Integer> pile;
+
+    public Fonction (int n, String nom, BlocDInstructions li, BlocDInstructions param) {
+        super(n);
+        this.nom = nom;
+        this.li = li;
+        this.param = param;
+        pile = Declaration.getInstance().getPileClone();
+      //  System.out.println(nom+pile.toString());
+    }
+
+    @Override
+    public String toMIPS() {
+        StringBuilder mips = new StringBuilder();
+        mips.append(nom+":\n");
+        mips.append("\tsw $ra, ($sp)\n");
+        mips.append("\taddi $sp, $sp, -4\n");
+        mips.append("\tli $v0, "+pile.get((pile.size()>=2) ? pile.size()-2 : pile.size()-1)+"\n");
+        mips.append("\tsw $v0, ($sp)\n");
+        mips.append("\taddi $sp, $sp, -4\n");
+        mips.append("\tli $v0, "+pile.peek()+"\n");
+        mips.append("\tsw, $v0, ($sp)\n");
+        int nbvar = Declaration.getInstance().getNbVars(pile.peek());
+        mips.append("\taddi $sp, $sp, "+(-4*nbvar+-4)+"\n");
+        mips.append(li.toMIPS());
+        mips.append("\taddi, $sp, $sp, -4\n");
+        mips.append("\tlw $ra, ($sp)\n");
+        mips.append("\taddi, $sp, $sp, 4\n");
+        mips.append("\tjr $ra\n");
+        mips.append(li.toMIPS());
+        return mips.toString();
+    }
+
+    @Override
+    public void verifier() {
+        boolean retourne = false;
+        Declaration.getInstance().doubleDeclFonc(nom, param.getSize(), pile);
+        Iterator<ArbreAbstrait> arbre = li.getInstruction();
+        ArbreAbstrait a = null;
+        while (arbre.hasNext()){
+            a = arbre.next();
+            a.verifier();
+            if (Declaration.getInstance().getRetourne()){
+                retourne = true;
+            }
+        }
+        System.out.println(retourne);
+        if (!a.getClass().toString().equals("class yal.arbre.instruction.Retourne") && retourne == false){
+            throw new AnalyseSemantiqueException(a.getNoLigne(), "La fonction " + nom + " doit se terminer par un l'instruction retourne");
+        }
+
+        li.verifier();
+    }
+
+    @Override
+    public String toString() {
+        return  "fonction " + nom + "() " + "debut " +  ((li != null) ? (li) : ("")) + " fin";
+    }
+}*/
+//}
 }
